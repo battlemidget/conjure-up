2c6344fcd51c351acace2161c9656444aac37c39
Adam Stokes <adam.stokes@ubuntu.com>
2016-07-27 17:41:07 -0400

    more spec updates
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjureup/spellkit/spec.py
+++ conjureup/spellkit/spec.py
@@ -1,16 +1,40 @@
 class Spec:
-    def __init__(self):
-        self.friendly_name = None
-        self.description = None
-        self.cloud_whitelist = []
-        self.packages = []
-        self.options_whitelist = {}
+    friendly_name = None
+    description = None
+    packages = []
+    options_whitelist = {}
+    _cloud_whitelist = []
+    _cloud_blacklist = []
 
-    def pre_deploy(self):
-        raise NotImplementedError
+    @property
+    def cloud_whitelist(self):
+        return self._cloud_whitelist
 
-    def is_deploy_done(self):
-        raise NotImplementedError
+    @property
+    def cloud_blacklist(self):
+        return self._cloud_blacklist
 
-    def post_deploy(self):
-        raise NotImplementedError
+    @cloud_whitelist.setter
+    def cloud_whitelist(self, val):
+        if self.cloud_blacklist:
+            raise Exception(
+                "In order to use whitelist the cloud blacklist must be empty.")
+        self._cloud_whitelist = val
+
+    @cloud_blacklist.setter
+    def cloud_blacklist(self, val):
+        if self.cloud_whitelist:
+            raise Exception(
+                "In order to use blacklist the cloud whitelist must be empty.")
+        self._cloud_blacklist = val
+
+    def __repr__(self):
+        return (
+            "<Friendly Name: {}, Description: {}, "
+            "Whitelist: {}, Blacklist: {}, Packages: {}, "
+            "Options-Whitelist: {}>".format(self.friendly_name,
+                                            self.description,
+                                            self.cloud_whitelist,
+                                            self.cloud_blacklist,
+                                            self.packages,
+                                            self.options_whitelist))
